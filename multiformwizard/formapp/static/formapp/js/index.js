1cc92eb3cb27185f3680ed148b4a4fb3b554cb70
Ennyola <medunoyeeni@gmail.com>
2022-03-17 08:13:57 +0100

    age validation and unique google ID added

--- multiformwizard/formapp/static/formapp/js/index.js
+++ multiformwizard/formapp/static/formapp/js/index.js
@@ -1,24 +1,54 @@
 let form = document.querySelector("#customerForm")
+let ageError = document.querySelector("#age-error")
+let biddingError = document.querySelector("#bd-error")
+let googleIDError = document.querySelector("#google_id_error")
+
 
 let AllBiddingSettings = ["HIGH", "MEDIUM", "LOW"]
+
+function underAgeValidate(birthday) {
+
+    // it will accept two types of format yyyy-mm-dd and yyyy/mm/dd
+    let optimizedBirthday = birthday.replace(/-/g, "/");
+
+    //set date based on birthday at 01:00:00 hours GMT+0100 (CET)
+    let myBirthday = new Date(optimizedBirthday);
+
+    // set current day on 01:00:00 hours GMT+0100 (CET)
+    let currentDate = new Date().toJSON().slice(0, 10) + ' 01:00:00';
+
+    // calculate age comparing current date and birthday
+    let myAge = ~~((Date.now(currentDate) - myBirthday) / (31557600000));
+
+    return myAge < 18 ? false : true;
+}
+
+
+
+
 form.addEventListener("submit", (e) => {
 
-    let d_o_b = form.elements['d_o_b'].value
+    let dateOfBirth = form.elements['d_o_b'].value
     let biddingSettings = form.elements['bd-settings'].value
-    let ageError = document.querySelector("#age-error")
-    let biddingError = document.querySelector("#bd-error")
+    let googleID = form.elements['google_id'].value
+
 
     // This ensures any previous error message is cleared
     ageError.innerHTML = ""
     biddingError.innerHTML = ""
+    googleIDError.innerHTML = ""
 
-    if (new Date().getFullYear() - d_o_b.split("-")[0] < 18) {
+    if (!underAgeValidate(dateOfBirth)) {
         ageError.innerHTML = "Must be 18 or above"
         e.preventDefault()
     }
-    if (!AllBiddingSettings.includes(biddingSettings.toUpperCase())) {
+    if (!AllBiddingSettings.includes(biddingSettings)) {
 
-        biddingError.innerHTML = "Invalid bidding settings"
+        biddingError.innerHTML = "Invalid bidding settings. Please Make sure they are all in uppercase"
+        e.preventDefault()
+    }
+    if (googleID.length !== 8) {
+        googleIDError.innerHTML = "Google Account ID is a set of 8 Numbers"
         e.preventDefault()
     }
 
--- multiformwizard/formapp/views.py
+++ multiformwizard/formapp/views.py
@@ -1,5 +1,7 @@
+import imp
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import UserApplication
 # Create your views here.
 
@@ -16,10 +18,12 @@ def index(request):
         bd_settings=request.POST.get("bd-settings",None)
         google_id=request.POST.get("google_id",None)
         company_name = request.POST.get("company_name",None)
-        application =  UserApplication(title=title,first_name=first_name,surname=surname,d_o_b=d_o_b,company_name=company_name,address=address,telephone=telephone,bidding_settings=bd_settings,google_id=google_id)
-        
-        application.save()
-        return redirect("applications")
+       
+        if UserApplication.objects.filter(google_id=google_id).exists():
+            messages.error(request, "An Application with that GOOGLE ID already exists")
+        else:
+            UserApplication.objects.create(title=title,first_name=first_name,surname=surname,d_o_b=d_o_b,company_name=company_name,address=address,telephone=telephone,bidding_settings=bd_settings.upper(),google_id=google_id)
+            return redirect("applications")
     
     return render(request, "formapp/customerSubmitForm.html")
 
